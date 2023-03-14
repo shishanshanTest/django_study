3acc4a6b628cca98b136ca9cccc89cee75347ef2
v_shanshi <v_shanshi@tencent.com>
2023-03-14 23:35:58 +0800

    MacBook第一次提交

--- django_study/settings.py
+++ django_study/settings.py
@@ -77,7 +77,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'django_study',
         'USER': 'root',
-        'PASSWORD': 'root',
+        'PASSWORD': 'password123',
         'HOST': '127.0.0.1',
         'PORT': '3306'
     }
